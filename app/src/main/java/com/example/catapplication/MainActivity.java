822cffeaa827071dc8c1cddcce392ef5ddd37475
nadiabenko <nadiabenkoo@gmail.com>
2020-11-16 23:49:52 +0700

    Add class Cat

--- app/src/main/java/com/example/catapplication/MainActivity.java
+++ app/src/main/java/com/example/catapplication/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.catapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Cat murzik = new Cat();
+        murzik.name = "Мурзик";
+        murzik.age = 9;
+        murzik.color = Color.BLACK;
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Log.i("GSON", gson.toJson(murzik));
     }
+
 }
